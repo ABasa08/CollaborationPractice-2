4a6d281a8ef8e38571403e0b6e65ef83ddd2ad54
ABasa08 <basa.anish08@gmail.com>
2025-07-18 18:18:09 -0700

    Test

--- calculator.py
+++ calculator.py
@@ -16,7 +16,7 @@ class Calculator:
 
 
 
-#assert Calculator.power(2.2, 3) == 10.648
+assert Calculator.power(2.2, 3) == 10.648
 #assert Calculator.multiply(2.4, 3.3) == 7.92
 #assert Calculator.divide(6.6, 2.2) == 3
 #assert Calculator.add(2.2, 3.3) == 5.5
